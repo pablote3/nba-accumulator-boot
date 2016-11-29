338b6edba2cf49bf9ad0749dbd418c5330a9d45c
Paul Rossotti <rossotti.paul@gmail.com>
2016-11-28 17:53:05 -0800

    initial commit

---
+++ src/main/java/com/rossotti/basketball/jpa/model/AbstractDomainClass.java
@@ -0,0 +1,51 @@
+package com.rossotti.basketball.jpa.model;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public class AbstractDomainClass implements ModelObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	@Enumerated(EnumType.STRING)
+	@Transient
+	private StatusCodeDAO statusCode;
+	public void setStatusCode(StatusCodeDAO statusCode) {
+		this.statusCode = statusCode;
+	}
+	public Boolean isFound() {
+		return statusCode == StatusCodeDAO.Found;
+	}
+	public Boolean isNotFound() {
+		return statusCode == StatusCodeDAO.NotFound;
+	}
+	public Boolean isUpdated() {
+		return statusCode == StatusCodeDAO.Updated;
+	}
+	public Boolean isCreated() {
+		return statusCode == StatusCodeDAO.Created;
+	}
+	public Boolean isDeleted() {
+		return statusCode == StatusCodeDAO.Deleted;
+	}
+
+	public enum StatusCodeDAO {
+		Found,
+		NotFound,
+		Updated,
+		Created,
+		Deleted
+	}
+}
---
+++ src/main/java/com/rossotti/basketball/jpa/model/ModelObject.java
@@ -0,0 +1,8 @@
+package com.rossotti.basketball.jpa.model;
+
+public interface ModelObject {
+    Long getId();
+    void setId(Long id);
+
+
+}
